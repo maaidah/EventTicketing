4390459dd2e57350469dd3460e1396aac9429b24
Maaidah Arif Siddiqui <77479098+maaidah@users.noreply.github.com>
2023-12-04 15:47:49 +0500

    Frontend4

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,32 +1,3 @@
-// import React from 'react';
-
-// function Homepage() {
-//   // Function to handle form submission
-//   const handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     // Your logic for handling form submission (e.g., sending data to a server)
-//     // Add your code here
-//   };
-
-//   return (
-//     <div>
-//       <h1>Event Ticketing Corner</h1>
-//       <form onSubmit={handleFormSubmit}>
-//         {/* Form inputs for ticket purchasing and management */}
-//         <label>
-//           Event Name:
-//           <input type="text" placeholder="Enter event name" />
-//         </label>
-//         {/* Other input fields (e.g., ticket quantity, payment info, etc.) */}
-//         <button type="submit">Purchase Tickets</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default Homepage;
-
-
 import React, { useState } from 'react';
 
 function Homepage() {
