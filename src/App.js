fcec0b357b3adfa143d436732ee0a66ec6f74276
Maaidah Arif Siddiqui <77479098+maaidah@users.noreply.github.com>
2023-12-04 15:20:14 +0500

    Frontend2

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Homepage from './Homepage'; // Import the Homepage component
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route exact path="/" component={Homepage} />
-//         {/* Other routes */}
-//       </Switch>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Homepage';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
         {/* Other routes */}
-      </Switch>
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-
-
