f23d10f160b3cd5e8ef31c148bb886086adf5e20
Maaidah Arif Siddiqui <77479098+maaidah@users.noreply.github.com>
2023-12-04 20:34:49 +0500

    Frontend7

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,7 +5,8 @@ function Homepage() {
   const [formData, setFormData] = useState({
     email: '',
     contactNumber: '',
-    ticketType: '', // No default ticket type initially
+    ticketType: '',
+    submitted: false,
   });
 
   const handleInputChange = (event) => {
@@ -18,56 +19,67 @@ function Homepage() {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    // Your logic for handling form submission with formData
-    console.log(formData); // For demonstration: log form data to the console
+    setFormData({ ...formData, submitted: true });
   };
 
   return (
     <div>
-      <h1>Event Ticketing Corner</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder="Enter email"
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Contact Number:
-          <input
-            type="tel"
-            name="contactNumber"
-            value={formData.contactNumber}
-            onChange={handleInputChange}
-            placeholder="Enter contact number"
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Ticket Type:
-          <select
-            name="ticketType"
-            value={formData.ticketType}
-            onChange={handleInputChange}
-            required // Make the field required
-          >
-            <option value="">Select an option</option> {/* Default option */}
-            <option value="concert">Concert</option>
-            <option value="sports">Sports</option>
-            <option value="theatre">Theatre Performance</option>
-            {/* Add more options as needed */}
-          </select>
-        </label>
-        <br />
-        <button type="submit">Purchase Tickets</button>
-      </form>
+      {!formData.submitted ? (
+        <div>
+          <h1>Event Ticketing Corner</h1>
+          <form onSubmit={handleFormSubmit}>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                placeholder="Enter email"
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Contact Number:
+              <input
+                type="tel"
+                name="contactNumber"
+                value={formData.contactNumber}
+                onChange={handleInputChange}
+                placeholder="Enter contact number"
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Ticket Type:
+              <select
+                name="ticketType"
+                value={formData.ticketType}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Select an option</option>
+                <option value="concert">Concert</option>
+                <option value="sports">Sports</option>
+                <option value="theatre">Theatre Performance</option>
+              </select>
+            </label>
+            <br />
+            <button type="submit">Purchase Tickets</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h1>Thank You!</h1>
+      <p>Your ticket purchase has been confirmed.</p>
+      <h2>Bill Summary</h2>
+      <p>Email: {formData.email}</p>
+      <p>Contact Number: {formData.contactNumber}</p>
+      <p>Ticket Type: {formData.ticketType}</p>
+        </div>
+      )}
     </div>
   );
 }
